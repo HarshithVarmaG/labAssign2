436ae06cd81fee1e565edcb7f46ec0ce82182429
HarshithVarmaG <160236297+HarshithVarmaG@users.noreply.github.com>
2024-02-22 20:37:55 +0530

    Add files via upload

---
+++ subtractionof2nums.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+int main() {
+   
+    int num1, num2, result;
+
+  
+    std::cout << "Enter the first number: ";
+    std::cin >> num1;
+
+   
+    std::cout << "Enter the second number: ";
+    std::cin >> num2;
+
+   
+    result = num1 - num2;
+
+    
+    std::cout << "The result of subtraction is: " << result << std::endl;
+
+    return 0;
+}
